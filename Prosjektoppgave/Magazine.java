e04a1b2f237b039f05fbd0ba2a7d7dafec1163e0
Draco25240 <Draco25240@hotmail.com>
2018-02-09 13:00:16 +0100

    Improved everything
    Improved basically everything from the original application

--- Prosjektoppgave/Magazine.java
+++ Prosjektoppgave/Magazine.java
@@ -1,6 +1,6 @@
 
 /**
- * Write a description of class Magazine here.
+ * Class that stores all relevant info for a magazine. 
  *
  * @author (your name)
  * @version (a version number or a date)
@@ -21,13 +21,13 @@ public class Magazine
         this.name = name;
         this.releaseNr = releaseNr;
         this.year = year;
-        this.month = month;
-        this.day = day;
+        this.month = month%12;
+        this.day = day%31;
     }
 
     /**
      * Returns the details of the magazine as a string of text.
-     * @return the details of the magazine as a string of text.
+     * @return details as string.
      */
     public String getDetailsAsString()
     {
@@ -36,7 +36,7 @@ public class Magazine
     
     /**
      * Returns the name of the magazine.
-     * @return the name of the magazine.
+     * @return name
      */
     public String getName()
     {
@@ -44,8 +44,17 @@ public class Magazine
     }
     
     /**
+     * Changes the name of the magazine
+     * @param 
+     */
+    public void setName(String newName)
+    {
+        this.name = newName;
+    }
+    
+    /**
      * Returns the number of the magazine.
-     * @return the number of the magazine.
+     * @return magazine number
      */
     public int getNumber()
     {
@@ -53,8 +62,17 @@ public class Magazine
     }
     
     /**
+     * Changes the number of the magazine.
+     * @param
+     */
+    public void setNumber(int newNumber)
+    {
+        this.releaseNr = newNumber;
+    }
+    
+    /**
      * Returns the year of the magazine's release.
-     * @return the year of the magazine's release.
+     * @return year
      */
     public int getYear()
     {
@@ -63,7 +81,7 @@ public class Magazine
     
     /**
      * Returns the month of the magazine's release.
-     * @return the month of the magazine's release.
+     * @return month
      */
     public int getMonth()
     {
@@ -72,7 +90,7 @@ public class Magazine
     
     /**
      * Returns the day of the magazine's release.
-     * @return the day of the magazine's release.
+     * @return day
      */
     public int getDay()
     {
--- Prosjektoppgave/MagazineStand.java
+++ Prosjektoppgave/MagazineStand.java
@@ -2,9 +2,10 @@ import java.util.ArrayList;
 import java.util.Iterator;
 /**
  * Write a description of class MagazineStand here.
+ * The MagazineClass class is where all the magazines are stored. ____
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Hans Christian Haugan Finnson, Fredrik Siem Taklo, Magnus Renaa Kjørseng
+ * @version 1.0
  */
 public class MagazineStand
 {
@@ -19,7 +20,7 @@ public class MagazineStand
     }
 
     /**
-     * 
+     * Adds a magazine to the magazine stand.
      */
     public void addMagazine(Magazine magazine)
     {
@@ -27,54 +28,94 @@ public class MagazineStand
     }
     
     /**
-     * 
+     * Removes a magazine from the magazine stand.
      */
-    public void removeMagazine(Magazine magazine)
+    public void removeMagazine(String magazineName,int magazineNumber)
     {
-       /*
-        
-       Iterator<Magazine> it = m.iterator();
-       while(it.hasNext())
+       boolean removed = false;
+       Iterator<Magazine> it = this.magazineArchive.iterator();
+       while(!removed && it.hasNext())
        {
            Magazine m = it.next();
-           String magazineName = m.getName();
-           if(magazineName.equals(magazine))
+           if((m.getName().equals(magazineName)) && (m.getNumber() == magazineNumber))
            {
-               
+               it.remove();
+               removed = true;
            }
        }
-       */
+       
     }
     
     /**
-     * 
+     * Changes the name and number of the given magazine.
      */
-    public String getMagazineByName(String magazineName)
+    public void changeMagazineDetails(String oldName,int oldNumber,String newName,int newNumber) 
     {
         Magazine foundMagazine = null;
-        int index = 0;
-        
-        while ((null == foundMagazine) && (index < this.magazineArchive.size()))
+        Iterator<Magazine> it = this.magazineArchive.iterator();
+        while ((null == foundMagazine) && (it.hasNext()))
         {
-            Magazine m = this.magazineArchive.get(index);
-            if (m.getName().equals(magazineName))
+            Magazine m = it.next();
+            if (m.getName().equals(oldName) && (m.getNumber() == oldNumber))
             {
+                foundMagazine = m;
+                m.setName(newName);
+                m.setNumber(newNumber);
+            }
+        }
+    } 
+    
+    /**
+     * Returns the number of magazines currently in the stand
+     * @return number of magazines
+     */
+    public int getNumberOfMagazines()
+    {
+        return magazineArchive.size();
+    } 
+    
+    /**
+     * Searches the magazine stand for a particular magazine
+     */
+    public Magazine getMagazine(String magazineName, int magazineNumber)
+    {
+        Magazine foundMagazine = null;
+        Iterator<Magazine> it = this.magazineArchive.iterator();
+        while ((null == foundMagazine) && (it.hasNext()))
+        {
+            Magazine m = it.next();
+            if (m.getName().equals(magazineName) && (m.getNumber() == magazineNumber))
+            {
+                System.out.println(m.getDetailsAsString());
                 foundMagazine = m;
             }
         }
-        
-        return magazineName;
+        return foundMagazine;
     }
     
     /**
-     * 
+     * Search the stand for a magazine series by its name, and display all magazines found in the terminal.
      */
-    public void listMagazines()
+    public void listMagazineSeries(String name)
+    {
+        for (Magazine m:this.magazineArchive)
+        {
+            if(m.getName().equals(name))
+            {
+                System.out.println(m.getDetailsAsString());
+            }
+        }
+    }
+    
+    /**
+     * List all magazines currently found in the magazine stand in the terminal.
+     */
+    public void listAllMagazines()
     {
        for (Magazine m:this.magazineArchive)
-        {
-            System.out.println(m.getDetailsAsString());
-        }
+       {
+           System.out.println(m.getDetailsAsString());
+       }
     }
     
     /**
@@ -84,18 +125,5 @@ public class MagazineStand
     {
        ;
     }
-    
-    /**
-     * 
-     */
-    public void viewMagazineSeries(String name)
-    {
-       for (Magazine m:this.magazineArchive)
-        {
-            if(m.getName().equals(name))
-            {
-                ;
-            }
-        }
-    }
+   
 }
